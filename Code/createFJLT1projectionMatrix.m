086225142e90069e6636a1e154ace3bd8458fa37
dvn14 <dvn14@ic.ac.uk>
2018-05-20 20:38:24 +0100

    work on testing FJLT1

--- Code/createFJLT1projectionMatrix.m
+++ Code/createFJLT1projectionMatrix.m
@@ -1,7 +1,8 @@
-function P = createFJLT1projectionMatrix(k,d,n,e)
+function [P,NZ] = createFJLT1projectionMatrix(k,d,n,e)
 %UNTITLED3 Summary of this function goes here
 %e=0.01;n=15;d=16;k=3;
 S = createSparseMatrix(e,n,d,2,k);
+NZ = nnz(S);
 H = createWalshHadamard(d,1);
 D = createRandomDiagonal(d);
 
---
+++ Code/test.m
@@ -0,0 +1,23 @@
+clear;
+n = 10; k = 32; d = 64; e = 0.4;
+A = rand(d,n);
+tmp = zeros([4,n]);
+[Phi,NZ] = (createFJLT1projectionMatrix(k,d,n,e));
+ct = zeros(1,n);
+for i = 1:n
+    x = A(:,i);
+    tic;
+    px = Phi*x;
+    ct(i) = toc;
+    tmp(1,i) = (1-e)*k*norm(x,2);
+    tmp(2,i) = norm(px,k);
+    tmp(3,i) = (1+e)*k*norm(x,2);
+    if ((tmp(2,i) >= tmp(1,i)) && (tmp(2,i) <= tmp(3,i)))
+        tmp(4,i) = 1;
+    else
+        tmp(4,i) = 0;
+    end
+end
+
+prob = sum(tmp(4,:))/n;
+O = d*log(d)+NZ;

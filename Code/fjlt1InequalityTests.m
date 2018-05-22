9a450872d7e781e12849dae12237b6bda2657371
dvn14 <dvn14@ic.ac.uk>
2018-05-22 18:15:51 +0100

    some work on FJLT 1

--- Code/fjlt1InequalityTests.m
+++ Code/fjlt1InequalityTests.m
@@ -3,12 +3,13 @@ n = 100; d = 128;
 A = rand(d,n);
 k = [2,4,8,16,32,64];
 k_dim = length(k);
-prob = zeros(1,50); averageCompTime = zeros(1,50); maxCompTime = zeros(1,50);
+prob = zeros(k_dim,50); averageCompTime = zeros(k_dim,50); maxCompTime = zeros(k_dim,50);
 probTwoThirdsAt = zeros(1,k_dim) -1;
-i = 4;
-%iteration for each k
-%for i = 1:k_dim
+aCTperK = zeros(1,k_dim);
+% iteration for each k
+for i = 1:k_dim
     %iteration for each e
+    disp(i);
     for e = 1:50
         %iteration to take the average
         prob_1 = 0;
@@ -20,37 +21,58 @@ i = 4;
             averageCompTime_1 = averageCompTime_1 + averageCompTime_2;
             averageMaxCompTime_1 = averageMaxCompTime_1 + averageMaxCompTime_2;
         end
-        prob(e) = (prob_1)/20;
-        averageCompTime(e) = (averageCompTime_1)/20;
-        maxCompTime(e) = (averageMaxCompTime_1)/20;
-        if ((prob(e) >= 2/3 ) && (probTwoThirdsAt(i) == -1))
+        prob(i,e) = (prob_1)/20;
+        averageCompTime(i,e) = (averageCompTime_1)/20;
+        maxCompTime(i,e) = (averageMaxCompTime_1)/20;
+        if ((prob(i,e) >= 2/3 ) && (probTwoThirdsAt(i) == -1))
             probTwoThirdsAt(i) = e/50;
         end
     end
-%end
+    aCTperK(i) = mean(averageCompTime(i,:));
+end
 
 e = 0.02:0.02:1;
 
 dir = 'results/';
 timestamp = datestr(now, 'dd-mm-yy_HH-MM-SS-FFF');
 
-figure()
-hold on
-yyaxis left
-plot(e,prob, 'b-', 'Markersize', 8);
-title(['Plot when n = ', num2str(n), ', d = ', num2str(d), ', and k = ', num2str(k(i))])
-xlabel('Error')
-ylabel('Average Probability')
+for i = 1:k_dim
+    figure()
+    hold on
+    yyaxis left
+    plot(e,prob(i,:), 'b-', 'Markersize', 8);
+    title(['Plot when n = ', num2str(n), ', d = ', num2str(d), ', and k = ', num2str(k(i))])
+    xlabel('Error')
+    ylabel('Average Probability')
 
-yyaxis right
-plot(e,averageCompTime, 'r-', 'Markersize', 8);
-ylabel('Average Computation Time (Sec)')
+    yyaxis right
+    plot(e,averageCompTime(i,:), 'r-', 'Markersize', 8);
+    ylabel('Average Computation Time (Sec)')
 
-legend({'Error','Average Computation Time'},'Location','northwest')
-hold off
-saveas(gca, strcat(dir, timestamp, ['_k_',num2str(k(i)),'.png']))
+    legend({'Error','Average Computation Time'},'Location','northwest')
+    hold off
+    saveas(gca, strcat(dir, timestamp, ['_k_',num2str(k(i)),'.png'])) 
+end
 
-%[prob, compTime, bigO, averageCompTime, projMatrix, nonZeros] = fjlt1_inequality(n,k,d,e,A);
+    figure()
+    hold on
+    yyaxis left
+    plot(k,probTwoThirdsAt, 'b-', 'Markersize', 8);
+    title(['Plot when n = ', num2str(n), ', d = ', num2str(d)])
+    xlabel('Embedded Dimension (k)')
+    ylabel('2/3 probability at error')
+
+    yyaxis right
+    plot(k,aCTperK, 'r-', 'Markersize', 8);
+    ylabel('Average Computation Time (Sec)')
+
+    legend({'Error','Average Computation Time'},'Location','northwest')
+    hold off
+    saveas(gca, strcat(dir, timestamp, ['_k_all','.png']))
+
+
+
+% [prob, compTime, bigO, averageCompTime, projMatrix, nonZeros] = fjlt1_inequality(n,k,d,e,A);
 
 fileID = fopen(strcat(dir, 'resultslog.txt'),'a');
 fprintf(fileID, '%21s | ', timestamp);

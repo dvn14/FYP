a37abf462b778ba7ea657b1588d1350875af31bd
dvn14 <dvn14@ic.ac.uk>
2018-06-02 01:43:22 +0100

    false structure part added

---
+++ Code/falsestructure.m
@@ -0,0 +1,20 @@
+clear;
+n = 5000;
+m = 10000;
+
+X = randn(n,m);
+
+EmpiricalCovMatrix = (1/m)*X*transpose(X);
+all_Eigenvalues     = eig(EmpiricalCovMatrix);
+rounded_Eigenvalues = round(all_Eigenvalues,2);
+eigenvalues = tabulate(rounded_Eigenvalues);
+
+dir = 'results/other/';
+timestamp = datestr(now, 'dd-mm-yy_HH-MM-SS-FFF');
+figure();
+hold on
+    histogram(all_Eigenvalues,[0 min(all_Eigenvalues):0.05:max(all_Eigenvalues)+0.05 3.5]);
+    title(['Eigenvalues of $$\hat{\sum}$$ for n = ', num2str(n), ', m = ', num2str(m)],'interpreter','latex')
+    xlabel('Eigenvalue')
+    hold off
+    saveas(gca, strcat(dir, timestamp, ['n_', num2str(n), '_m_', num2str(m),'.png']))
